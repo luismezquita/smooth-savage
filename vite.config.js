fbd172738a201ae369d9150814e07a3060b52eb4
Luis Fernando <luismezquita@gmail.com>
2026-05-18 09:08:58 +0200

    PWA: runtime caching, maskable icon, screenshots, manifest id

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'icons/*.png', 'images/**/*'],
       manifest: {
+        id: '/',
         name: 'Smooth Savage',
         short_name: 'Smooth Savage',
         description: 'Fresh foods, superfoods and smoothie recipes',
@@ -25,14 +26,43 @@ export default defineConfig({
           {
             src: '/icons/rayo-hoja-512.png',
             sizes: '512x512',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ],
+        screenshots: [
+          {
+            src: '/images/screenshots/fotosmoothie.png',
+            sizes: '750x1334',
+            type: 'image/png',
+            form_factor: 'narrow'
+          },
+          {
+            src: '/images/screenshots/fotofresh.png',
+            sizes: '750x1334',
+            type: 'image/png',
+            form_factor: 'narrow'
           }
         ]
       },
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,jpg,webp,woff2,svg}'],
+        globPatterns: ['**/*.{js,css,html,ico,woff2,svg}'],
+        globIgnores: ['**/images/**'],
         navigateFallback: '/index.html',
-        navigateFallbackDenylist: [/^\/api\//]
+        navigateFallbackDenylist: [/^\/api\//],
+        runtimeCaching: [
+          {
+            urlPattern: /\/images\//,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'images-cache',
+              expiration: {
+                maxEntries: 200,
+                maxAgeSeconds: 60 * 60 * 24 * 30
+              }
+            }
+          }
+        ]
       }
     })
   ],
